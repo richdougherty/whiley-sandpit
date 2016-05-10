ebc7742367c36a7cf48b62a9781d93df0557b5ae
Rich Dougherty <rich@rd.gen.nz>
2016-05-10 15:25:20 +1200

    More work on simplifying types

--- src/main/scala/nz/rd/whiley/Intersection.scala
+++ src/main/scala/nz/rd/whiley/Intersection.scala
@@ -7,12 +7,33 @@ import scala.reflect.ClassTag
 import nz.rd.whiley.Graph.{ Id, Node }
 
 final class IntersectionAlgorithm(
-    val g: Graph, intersections: mutable.Map[(Id, Id), Intersections]) {
+    val g: Graph,
+    val contents: mutable.Map[Id, Contents],
+    val intersections: mutable.Map[(Id, Id), Intersections]) {
 
-  def get(a: Id, b: Id): Intersections = {
+  def getInts(a: Id, b: Id): Intersections = {
     // Normalise key order
-    val key: (Id, Id) = if (a <= b) (a, b) else (b, a)
-    intersections.getOrElseUpdate(key, Intersections())
+    if (a <= b) {
+      intersections.getOrElseUpdate((a, b), Intersections())
+    } else {
+      // Look up with flipped keys, then use flipped view of intersection
+      intersections.getOrElseUpdate((b, a), Intersections()).flipped
+    }
+  }
+
+  def getContents(a: Id): Contents = {
+    contents.getOrElseUpdate(a, new Contents())
+  }
+
+  /**
+   * Iterate on mutable state until a fixpoint is reached.
+   */
+  @tailrec
+  private def fixpoint(readState: => Any)(body: => Any): Unit = {
+    val before = readState
+    body
+    val after = readState
+    if (before != after) { fixpoint(readState)(body) }
   }
 
   private def pairs[A](seq: Iterable[A]): Iterable[(A, A)] = {
@@ -33,7 +54,7 @@ final class IntersectionAlgorithm(
         val cls: Class[N] = implicitly[ClassTag[N]].runtimeClass.asInstanceOf[Class[N]]
         val n: Node = g(a)
         if (cls.isInstance(n)) {
-          val ints: Intersections = get(a, b)
+          val ints: Intersections = getInts(a, b)
           Some((ints, a, n.asInstanceOf[N], b))
         } else {
           None
@@ -43,52 +64,222 @@ final class IntersectionAlgorithm(
     }
   }
   object Has {
+    object Any extends Has[Node.Any.type]
+    object Void extends Has[Node.Void.type]
     object Union extends Has[Node.Union]
     object Negation extends Has[Node.Negation]
   }
 
+  object AnyNodes {
+    def unapply(p: (Id, Id)): Option[(Intersections, Id, Node, Id, Node)] = {
+      val ints: Intersections = getInts(p._1, p._2)
+      val n1: Node = g(p._1)
+      val n2: Node = g(p._2)
+      Some((ints, p._1, n1, p._2, n2))
+    }
+  }
+
   def calculate(): Unit = {
 
-    @tailrec
-    def loop(): Unit = {
-      def saveState: List[((Id, Id), (Option[Size], Option[Size], Option[Size], Option[Size]))] = {
+    fixpoint {
+      (g.root, g.nodes.toList)
+    } {
+
+      // Simple, structural simplifications
+
+      fixpoint {
+        (g.root, g.nodes.toList)
+      } {
+        for ((id, node) <- g.nodes) {
+          node match {
+            case Node.Any => ()
+            case Node.Void => ()
+            case Node.Int => ()
+            case Node.Union(children) =>
+              if (children.exists(g.nodes(_) == Node.Any)) {
+                // If there's an Any node, then replace Union with Any
+                g.nodes(id) = Node.Any
+              } else {
+                // Process and combine nested unions, removing inter-union links
+                val unions = mutable.Set[Id]()
+                def tidy(childrenToTidy: List[Id]): List[Id] = {
+                  childrenToTidy.map(cid => (cid, g.nodes(cid))).flatMap {
+                    case (cid, _) if unions.contains(cid) =>
+                      // Union already processed
+                      Seq.empty
+                    case (_, Node.Void) =>
+                      // Void node can be omitted
+                      Seq.empty
+                    case (cid, Node.Union(childChildren)) =>
+                      // Add union to set of processed unions; merge its children
+                      unions += cid
+                      tidy(childChildren)
+                    case (cid, _) =>
+                      Seq(cid)
+                  }
+                }
+                unions += id
+                val newChildren = tidy(children)
+                val newNode = newChildren match {
+                  case Nil => Node.Void
+                  case child::Nil => g.nodes(child)
+                  case _ => Node.Union(newChildren)
+                }
+                g.nodes(id) = newNode
+              }
+            case Node.Negation(child) =>
+              val childNode = g.nodes(child)
+              childNode match {
+                case Node.Any =>
+                  g(id) = Node.Void
+                case Node.Void =>
+                  g(id) = Node.Any
+                case Node.Negation(grandchild) =>
+                  // Flatten double negation
+                  g(id) = g(grandchild)
+                case _ =>
+                  ()
+              }
+          }
+        }
+      }
+
+      // Calculate emptiness of nodes and their negations
+
+      fixpoint {
+        contents.toList.map {
+          case (key, contents) => (key, (contents.p, contents.n))
+        }
+      } {
+        for ((id, node) <- g.nodes) {
+          val conts = getContents(id)
+          node match {
+            case Node.Any =>
+              conts.p = Some(NonEmpty)
+              conts.n = Some(Empty)
+            case Node.Void =>
+              conts.p = Some(Empty)
+              conts.n = Some(NonEmpty)
+            case Node.Int =>
+              conts.p = Some(NonEmpty)
+              conts.n = Some(NonEmpty)
+            case Node.Union(children) =>
+              val childrenContent = children.map(getContents(_))
+              if (childrenContent.exists(_.p == Some(NonEmpty))) { conts.p = Some(NonEmpty) }
+              if (childrenContent.forall(_.n == Some(Empty))) { conts.n = Some(Empty) }
+            case Node.Negation(child) =>
+              val childContent = getContents(child)
+              conts.p = childContent.n
+              conts.n = childContent.p
+          }
+        }
+      }
+
+      // Calculate emptiness of intersections between nodes and their negations
+
+      fixpoint {
         intersections.toList.map {
           case (key, ints) => (key, (ints.pp, ints.pn, ints.np, ints.nn))
         }
-      }
-      val before = saveState
-      for (((a: Id, an: Node), (b: Id, bn: Node)) <- pairs(g.nodes)) {
-        (a, b) match {
-          case (_, _) if an == bn =>
-            val ints = get(a, b)
-            ints.pp = Some(if (an == Node.Void) Empty else NonEmpty)
-            ints.pn = Some(Empty)
-            ints.np = Some(Empty)
-            ints.nn = Some(if (an == Node.Any) Empty else NonEmpty)
-          case Has.Negation(ints, id, Node.Negation(c), oid) =>
-            val childInts = get(c, oid)
-            ints.pp = childInts.pp.map(_.inverse)
-            ints.pn = childInts.pn.map(_.inverse)
-            ints.np = childInts.np.map(_.inverse)
-            ints.nn = childInts.nn.map(_.inverse)
-          case Has.Union(ints, id, Node.Union(children), oid) =>
-            val childrenInts = children.map(get(_, oid))
-            if (childrenInts.exists(_.pp == Some(NonEmpty))) { ints.pp = Some(NonEmpty) }
-            if (childrenInts.exists(_.pn == Some(NonEmpty))) { ints.pn = Some(NonEmpty) }
-            if (childrenInts.forall(_.np == Some(Empty))) { ints.np = Some(Empty) }
-            if (childrenInts.forall(_.nn == Some(Empty))) { ints.nn = Some(Empty) }
+      } {
+        for (((a: Id, an: Node), (b: Id, bn: Node)) <- pairs(g.nodes)) {
+          (a, b) match {
+
+            // Types with nodes that are the same
+
+            case AnyNodes(ints, aid, anode, bid, bnode) if anode == bnode =>
+              val ints = getInts(a, b)
+              val contents = getContents(a)
+              ints.pp = contents.p
+              ints.pn = Some(Empty)
+              ints.np = Some(Empty)
+              ints.nn = contents.n
+
+            // Types with intersections derived from their children
+
+            case Has.Negation(ints, id, Node.Negation(c), oid) =>
+              val childInts = getInts(c, oid)
+              ints.pp = childInts.np
+              ints.pn = childInts.nn
+              ints.np = childInts.pp
+              ints.nn = childInts.pn
+            case Has.Union(ints, id, Node.Union(children), oid) =>
+              val childrenInts = children.map(getInts(_, oid))
+              if (childrenInts.exists(_.pp == Some(NonEmpty))) { ints.pp = Some(NonEmpty) }
+              if (childrenInts.exists(_.pn == Some(NonEmpty))) { ints.pn = Some(NonEmpty) }
+              if (childrenInts.forall(_.np == Some(Empty))) { ints.np = Some(Empty) }
+              if (childrenInts.forall(_.nn == Some(Empty))) { ints.nn = Some(Empty) }
+
+            // Types with intersections derived from the other term
+
+            case Has.Any(ints, id, Node.Any, oid) =>
+              val otherContents = getContents(id)
+              ints.pp = otherContents.p
+              ints.pn = otherContents.n
+              ints.np = Some(Empty)
+              ints.nn = Some(Empty)
+            case Has.Void(ints, id, Node.Void, oid) =>
+              val otherContents = getContents(id)
+              ints.pp = Some(Empty)
+              ints.pn = Some(Empty)
+              ints.np = otherContents.p
+              ints.nn = otherContents.n
+
+            case _ =>
+              throw new MatchError(s"Failed to match $a (${g(a)}) and $b (${g(b)})")
+          }
         }
       }
-      val after = saveState
-      if (before != after) { loop() } else {
-        println(g.nodes)
-        println(after)
+
+      // TODO: Use intersection information to find equal types
+
+      // Use intersection information to simplify unions
+
+      fixpoint {
+        (g.root, g.nodes.toList)
+      } {
+        for ((id, node) <- g.nodes) {
+          node match {
+            case Node.Union(children) =>
+              val removals = mutable.ArrayBuffer[Id]()
+              val additions = mutable.ArrayBuffer[Id]()
+              val f: ((Id, Id)) => Unit = {
+                case (a, b) if a == b =>
+                  // Don't process self-intersections
+                  ()
+                case AnyNodes(ints, aid, anode, bid, bnode) =>
+                  (ints.pp, ints.pn, ints.np, ints.nn) match {
+                    case (_, _, _, Some(Empty)) => // union of both sets is Any
+                      removals += aid
+                      removals += bid
+                      additions += (g += Node.Any)
+                    case (_, _, Some(Empty), _) => // a > b - remove b
+                      removals += bid
+                    case (_, Some(Empty), _, _) => // b > a - remove a
+                      removals += aid
+                  }
+                  ()
+              }
+              pairs(children).foreach(f)
+              val newChildren = children.filter(c => !removals.contains(c)) ++ additions
+              g(id) = Node.Union(newChildren)
+
+            case _ => ()
+          }
+        }
       }
     }
 
-    loop()
   }
 
+}
+
+// sealed trait PairMatch
+// case class SelfMatch(id: Id) extends PairMatch
+// case class OneMatch(id: Id, ints: Intersections, node: Node, other: Id) extends PairMatch
+
+object IntersectionAlgorithm {
+  def forGraph(g: Graph) = new IntersectionAlgorithm(g, mutable.Map.empty, mutable.Map.empty)
 }
 
 trait Intersections {
@@ -96,7 +287,8 @@ trait Intersections {
   var pn: Option[Size]
   var np: Option[Size]
   var nn: Option[Size]
-  def flip: Intersections
+  override def toString: String = s"Intersections(pp: $pp, pn: $pn, np: $np, nn: $nn)"
+  def flipped: Intersections
 }
 
 object Intersections {
@@ -106,8 +298,8 @@ object Intersections {
     override var pn: Option[Size] = None
     override var np: Option[Size] = None
     override var nn: Option[Size] = None
-    override def flip: Intersections = new Intersections {
-      override def flip: Intersections = canonical
+    override def flipped: Intersections = new Intersections {
+      override def flipped: Intersections = canonical
         def pp: Option[Size] = canonical.pp
         def pp_=(value: Option[Size]): Unit = canonical.pp = value
         def pn: Option[Size] = canonical.np
@@ -118,6 +310,12 @@ object Intersections {
         def nn_=(value: Option[Size]): Unit = canonical.nn = value
     }
   }
+}
+
+class Contents {
+  var p: Option[Size] = None
+  var n: Option[Size] = None
+  override def toString: String = s"Contents(p: $p, n: $n)"
 }
 
 sealed trait Size {
--- src/test/scala/nz/rd/whiley/IntersectionSpec.scala
+++ src/test/scala/nz/rd/whiley/IntersectionSpec.scala
@@ -9,37 +9,137 @@ class IntersectionSpec extends FreeSpec with Matchers {
 
     "of the root with itself" - {
 
-      def runIntersection(t: Tree): (Tree, Intersections) = {
-        val alg = new IntersectionAlgorithm(Graph.fromTree(t), mutable.Map.empty)
+      def runIntersection(t: Tree): (Tree, Contents, Intersections) = {
+        println(s"--- Running intersection on $t ---")
+        val alg = IntersectionAlgorithm.forGraph(Graph.fromTree(t))
         alg.calculate()
+        println(s"Alg: nodes: ${alg.g.root}, ${alg.g.nodes}, ${alg.contents}, ${alg.intersections}")
         val tree = alg.g.toTree
-        val ints = alg.get(alg.g.root, alg.g.root)
-        (tree, ints)
+        val rootConts = alg.getContents(alg.g.root)
+        val rootInts = alg.getInts(alg.g.root, alg.g.root)
+        println(s"--- Done ---")
+        (tree, rootConts, rootInts)
       }
 
       "should handle the 'any' type" in {
-        val (tree, ints) = runIntersection(Tree.Any)
+        val (tree, conts, ints) = runIntersection(Tree.Any)
         tree should be (Tree.Any)
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(Empty))
         ints.pp should be(Some(NonEmpty))
         ints.pn should be(Some(Empty))
         ints.np should be(Some(Empty))
         ints.nn should be(Some(Empty))
       }
       "should handle the 'void' type" in {
-        val (tree, ints) = runIntersection(Tree.Void)
+        val (tree, conts, ints) = runIntersection(Tree.Void)
         tree should be(Tree.Void)
+        conts.p should be(Some(Empty))
+        conts.n should be(Some(NonEmpty))
         ints.pp should be(Some(Empty))
         ints.pn should be(Some(Empty))
         ints.np should be(Some(Empty))
         ints.nn should be(Some(NonEmpty))
       }
       "should handle the 'int' type" in {
-        val (tree, ints) = runIntersection(Tree.Int)
+        val (tree, conts, ints) = runIntersection(Tree.Int)
         tree should be(Tree.Int)
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(NonEmpty))
         ints.pp should be(Some(NonEmpty))
         ints.pn should be(Some(Empty))
         ints.np should be(Some(Empty))
         ints.nn should be(Some(NonEmpty))
+      }
+      "should handle the '!any' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Negation(Tree.Any))
+        tree should be(Tree.Void)
+        conts.p should be(Some(Empty))
+        conts.n should be(Some(NonEmpty))
+        ints.pp should be(Some(Empty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(NonEmpty))
+      }
+      "should handle the '!void' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Negation(Tree.Void))
+        tree should be(Tree.Any)
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(Empty))
+        ints.pp should be(Some(NonEmpty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(Empty))
+      }
+      "should handle the '!int' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Negation(Tree.Int))
+        tree should be(Tree.Negation(Tree.Int))
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(NonEmpty))
+        ints.pp should be(Some(NonEmpty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(NonEmpty))
+      }
+      "should handle the '!!int' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Negation(Tree.Negation(Tree.Int)))
+        tree should be(Tree.Int)
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(NonEmpty))
+        ints.pp should be(Some(NonEmpty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(NonEmpty))
+      }
+      "should handle the '|' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Union(Nil))
+        tree should be(Tree.Void)
+        conts.p should be(Some(Empty))
+        conts.n should be(Some(NonEmpty))
+        ints.pp should be(Some(Empty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(NonEmpty))
+      }
+      "should handle the 'any|' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Union(List(Tree.Any)))
+        tree should be(Tree.Any)
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(Empty))
+        ints.pp should be(Some(NonEmpty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(Empty))
+      }
+      "should handle the 'void|' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Union(List(Tree.Void)))
+        tree should be(Tree.Void)
+        conts.p should be(Some(Empty))
+        conts.n should be(Some(NonEmpty))
+        ints.pp should be(Some(Empty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(NonEmpty))
+      }
+      "should handle the 'int|' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Union(List(Tree.Int)))
+        tree should be(Tree.Int)
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(NonEmpty))
+        ints.pp should be(Some(NonEmpty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(NonEmpty))
+      }
+      "should handle the 'int|!int' type" in {
+        val (tree, conts, ints) = runIntersection(Tree.Union(List(Tree.Int, Tree.Negation(Tree.Int))))
+        tree should be(Tree.Any)
+        conts.p should be(Some(NonEmpty))
+        conts.n should be(Some(Empty))
+        ints.pp should be(Some(NonEmpty))
+        ints.pn should be(Some(Empty))
+        ints.np should be(Some(Empty))
+        ints.nn should be(Some(Empty))
       }
 
     }
@@ -47,7 +147,7 @@ class IntersectionSpec extends FreeSpec with Matchers {
     "of unions" - {
 
       "should handle the 'int|!int' type" in {
-        val alg = new IntersectionAlgorithm(Graph.fromTree(Tree.Union(List(Tree.Int, Tree.Negation(Tree.Int)))), mutable.Map.empty)
+        val alg = IntersectionAlgorithm.forGraph(Graph.fromTree(Tree.Union(List(Tree.Int, Tree.Negation(Tree.Int)))))
         alg.calculate()
       }
 
