d8fc23bc9eb5a760ab9b262bba9c9879e15d4298
Rich Dougherty <rich@rd.gen.nz>
2016-07-19 10:36:52 +1200

    More static type check tests

--- src/test/scala/nz/rd/whiley/StaticTypeCheckerSpec.scala
+++ src/test/scala/nz/rd/whiley/StaticTypeCheckerSpec.scala
@@ -12,39 +12,56 @@ class StaticTypeCheckerSpec extends FreeSpec with Matchers {
 
   "StaticTypeChecker" - {
 
-    "should typecheck null" in {
+    "should typecheck null as root:null" in {
       check(Tree.Null) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Null))))
     }
-    "should typecheck !null" in {
+    "should typecheck !null as !root:null" in {
       check(Tree.Negation(Tree.Null)) should be(Set(DNF.Disj(DNF.Term.isNotKind(DNF.RootVal, DNF.Kind.Null))))
     }
-    "should typecheck int" in {
+    "should typecheck int as root:int" in {
       check(Tree.Int) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))))
     }
-    "should typecheck !int" in {
+    "should typecheck !int as !root:int" in {
       check(Tree.Negation(Tree.Int)) should be(Set(DNF.Disj(DNF.Term.isNotKind(DNF.RootVal, DNF.Kind.Int))))
     }
-    "should typecheck void" in {
+    "should typecheck !!int as root:int" in {
+      check(Tree.Negation(Tree.Negation(Tree.Int))) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))))
+    }
+    "should typecheck void as false" in {
       check(Tree.Void) should be(Set(DNF.Disj.False))
     }
-    "should typecheck any" in {
+    "should typecheck any as true" in {
       check(Tree.Any) should be(Set(DNF.Disj.True))
     }
-    "should typecheck !void" in {
+    "should typecheck !void as true" in {
       check(Tree.Negation(Tree.Void)) should be(Set(DNF.Disj.True))
     }
-    "should typecheck !any" in {
+    "should typecheck !any as false" in {
       check(Tree.Negation(Tree.Any)) should be(Set(DNF.Disj.False))
     }
-    "should typecheck void|int" in {
+    "should typecheck void|int as root:int" in {
       check(Tree.Union(List(Tree.Void, Tree.Int))) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))))
     }
-    "should typecheck null|int" in {
+    "should typecheck null|int as root:null | root:int" in {
       check(Tree.Union(List(Tree.Null, Tree.Int))) should be(Set(
         DNF.Disj(
           DNF.Conj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Null)),
           DNF.Conj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))
         )
+      ))
+    }
+    "should typecheck µX.!X as unknown" in {
+      check(Tree.Recursive("X", Tree.Negation(Tree.Variable("X")))) should be(Set(DNF.Disj(TUnknown)))
+    }
+    "should typecheck µX.(X|) as unknown" in {
+      check(Tree.Recursive("X", Tree.Union(List(Tree.Variable("X"))))) should be(Set(DNF.Disj(TUnknown)))
+    }
+    "should typecheck !µX.(X|) as unknown" in {
+      check(Tree.Negation(Tree.Recursive("X", Tree.Union(List(Tree.Variable("X")))))) should be(Set(DNF.Disj(TUnknown)))
+    }
+    "should typecheck µX.X|int as unknown | root:int" in {
+      check(Tree.Recursive("X", Tree.Union(List(Tree.Variable("X"), Tree.Int)))) should be(Set(
+        DNF.Disj(TUnknown) | DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))
       ))
     }
   }
