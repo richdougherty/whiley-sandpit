59e5625651663a6a8d6a84e23c0cf8256acfb607
Rich Dougherty <rich@rd.gen.nz>
2016-07-22 13:34:43 +1200

    Better names for test cases

--- src/test/scala/nz/rd/whiley/StaticTypeCheckerSpec.scala
+++ src/test/scala/nz/rd/whiley/StaticTypeCheckerSpec.scala
@@ -16,40 +16,40 @@ class StaticTypeCheckerSpec extends FreeSpec with Matchers {
 
   "StaticTypeChecker" - {
 
-    "should typecheck null as root:null" in {
+    "should convert root:null to root:null" in {
       check(Tree.Null) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Null))))
     }
-    "should typecheck !null as !root:null" in {
+    "should convert root:!null to !root:null" in {
       check(Tree.Negation(Tree.Null)) should be(Set(DNF.Disj(DNF.Term.isNotKind(DNF.RootVal, DNF.Kind.Null))))
     }
-    "should typecheck int as root:int" in {
+    "should convert root:int to root:int" in {
       check(Tree.Int) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))))
     }
-    "should typecheck !int as !root:int" in {
+    "should typecheck !int to !root:int" in {
       check(Tree.Negation(Tree.Int)) should be(Set(DNF.Disj(DNF.Term.isNotKind(DNF.RootVal, DNF.Kind.Int))))
     }
-    "should typecheck !!int as root:int" in {
+    "should convert root:!!int to root:int" in {
       check(Tree.Negation(Tree.Negation(Tree.Int))) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))))
     }
-    "should typecheck void as false" in {
+    "should convert root:void to false" in {
       check(Tree.Void) should be(Set(DNF.Disj.False))
     }
-    "should typecheck any as true" in {
+    "should convert root:any to true" in {
       check(Tree.Any) should be(Set(DNF.Disj.True))
     }
-    "should typecheck !void as true" in {
+    "should convert root:!void to true" in {
       check(Tree.Negation(Tree.Void)) should be(Set(DNF.Disj.True))
     }
-    "should typecheck !any as false" in {
+    "should convert root:!any to false" in {
       check(Tree.Negation(Tree.Any)) should be(Set(DNF.Disj.False))
     }
-    "should typecheck any|int as true" in {
+    "should convert root:(any|int) to true" in {
       check(Tree.Union(List(Tree.Any, Tree.Int))) should be(Set(DNF.Disj(TTrue)))
     }
-    "should typecheck void|int as root:int" in {
+    "should convert root:(void|int) to root:int" in {
       check(Tree.Union(List(Tree.Void, Tree.Int))) should be(Set(DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))))
     }
-    "should typecheck null|int as root:null | root:int" in {
+    "should convert root:(null|int) to (root:null | root:int)" in {
       check(Tree.Union(List(Tree.Null, Tree.Int))) should be(Set(
         DNF.Disj(
           DNF.Conj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Null)),
@@ -57,76 +57,76 @@ class StaticTypeCheckerSpec extends FreeSpec with Matchers {
         )
       ))
     }
-    "should typecheck any&int as root:int" in {
+    "should convert root:(any&int) to root:int" in {
       check(Tree.Intersection(List(Tree.Any, Tree.Int))) should be(Set(
         DNF.Disj(TTrue) & DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))
       ))
     }
-    "should typecheck void&int as false" in {
+    "should convert root:(void&int) to false" in {
       check(Tree.Intersection(List(Tree.Void, Tree.Int))) should be(Set(DNF.Disj(TFalse)))
     }
-    "should typecheck null&int as false" in {
+    "should convert root:(null&int) to false" in {
       check(Tree.Intersection(List(Tree.Null, Tree.Int))) should be(Set(DNF.Disj(TFalse)))
     }
-    "should typecheck null&<int> as false" in {
+    "should convert root:(null&<int>) to false" in {
       check(Tree.Intersection(List(Tree.Null, Tree.Product(List(Tree.Int))))) should be(Set(DNF.Disj(TFalse)))
     }
-    "should typecheck µX.!X as unknown" in {
+    "should convert root:(µX.!X) to unknown" in {
       check(Tree.Recursive("X", Tree.Negation(Tree.Variable("X")))) should be(Set(DNF.Disj(TUnknown)))
     }
-    "should typecheck !(µX.!X) as unknown" in {
+    "should convert root:!(µX.!X) to unknown" in {
       check(Tree.Negation(Tree.Recursive("X", Tree.Negation(Tree.Variable("X"))))) should be(Set(DNF.Disj(TUnknown)))
     }
-    "should typecheck µX.(X|) as unknown" in {
+    "should convert root:µX.(X|) to unknown" in {
       check(Tree.Recursive("X", Tree.Union(List(Tree.Variable("X"))))) should be(Set(DNF.Disj(TUnknown)))
     }
-    "should typecheck µX.(X&) as unknown" in {
+    "should convert root:(µX.(X&)) to unknown" in {
       check(Tree.Recursive("X", Tree.Intersection(List(Tree.Variable("X"))))) should be(Set(DNF.Disj(TUnknown)))
     }
-    "should typecheck !µX.(X&) as unknown" in {
+    "should convert root:(!µX.(X&)) to unknown" in {
       check(Tree.Negation(Tree.Recursive("X", Tree.Intersection(List(Tree.Variable("X")))))) should be(Set(DNF.Disj(TUnknown)))
     }
-    "should typecheck µX.!(X|) as unknown" in {
+    "should convert root:(µX.!(X|)) to unknown" in {
       check(Tree.Recursive("X", Tree.Negation(Tree.Union(List(Tree.Variable("X")))))) should be(Set(DNF.Disj(TUnknown)))
     }
-    "should typecheck µX.!(X&) as unknown" in {
+    "should convert root:(µX.!(X&)) to unknown" in {
       check(Tree.Recursive("X", Tree.Negation(Tree.Intersection(List(Tree.Variable("X")))))) should be(Set(DNF.Disj(TUnknown)))
     }
-    "should typecheck µX.X|int as unknown | root:int" in {
+    "should convert root:(µX.(X|int)) to (unknown | root:int)" in {
       check(Tree.Recursive("X", Tree.Union(List(Tree.Variable("X"), Tree.Int)))) should be(Set(
         DNF.Disj(TUnknown) | DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))
       ))
     }
-    "should typecheck µX.X&int as unknown & root:int" in {
+    "should convert root:(µX.(X&int)) to (unknown & root:int)" in {
       check(Tree.Recursive("X", Tree.Intersection(List(Tree.Variable("X"), Tree.Int)))) should be(Set(
         DNF.Disj(TUnknown) & DNF.Disj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Int))
       ))
     }
-    "should typecheck <int> as root:product(1) & root[0]:int" in {
+    "should convert root:<int> to (root:product(1) & root[0]:int)" in {
       check(Tree.Product(List(Tree.Int))) should be(Set(
         DNF.Disj(DNF.Conj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Product(1)), DNF.Term.isKind(DNF.ProductVal(DNF.RootVal, 0), DNF.Kind.Int)))
       ))
     }
-    "should typecheck <!int> as root:product(1) & !root[0]:int" in {
+    "should convert root:<!int> to (root:product(1) & !root[0]:int)" in {
       check(Tree.Product(List(Tree.Negation(Tree.Int)))) should be(Set(
         DNF.Disj(DNF.Conj(DNF.Term.isKind(DNF.RootVal, DNF.Kind.Product(1)), DNF.Term.isNotKind(DNF.ProductVal(DNF.RootVal, 0), DNF.Kind.Int)))
       ))
     }
-    "should typecheck µX.<X> as false" in {
+    "should convert root:(µX.<X>) to false" in {
       check(Tree.Recursive("X", Tree.Product(List(Tree.Variable("X"))))) should be(Set(
         DNF.Disj(TFalse)
       ))
     }
-    "possible µX.<X> typecheck values should be {false}" in {
+    "superset of possible results for root:(µX.<X>) should be {false}" in {
       possibleValues(Tree.Recursive("X", Tree.Product(List(Tree.Variable("X"))))) should be(Set(TFalse))
     }
-    "possible µX.<!X> typecheck values should be {true, false}" in {
+    "superset of possible results for root:(µX.<!X>) should be {true, false}" in {
       possibleValues(Tree.Recursive("X", Tree.Product(List(Tree.Negation(Tree.Variable("X")))))) should be(Set(TTrue, TFalse))
     }
-    "possible µX.(null|<int,X>) typecheck values should be {true, false}" in {
+    "superset of possible results for root:(µX.(null|<int,X>)) should be {true, false}" in {
       possibleValues(Tree.Recursive("X", Tree.Union(List(Tree.Null, Tree.Product(List(Tree.Int, Tree.Variable("X"))))))) should be(Set(TTrue, TFalse))
     }
-    "possible µX.(null&<int,X>) typecheck values should be {false}" in {
+    "superset of possible results for root:(µX.(null&<int,X>)) should be {false}" in {
       possibleValues(Tree.Recursive("X", Tree.Intersection(List(Tree.Null, Tree.Product(List(Tree.Int, Tree.Variable("X"))))))) should be(Set(TFalse))
     }
   }
